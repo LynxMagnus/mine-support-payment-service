197dfc8bb50ea17af660963e2ff02bb934aa10ef
John Watson <john.watson2@rpa.gov.uk>
2019-06-28 15:57:48 +0100

    Message service unavailability handled

--- server/services/message-service.js
+++ server/services/message-service.js
@@ -8,30 +8,31 @@ module.exports = {
     amqp.connect(messageQueue, function (err, conn) {
       if (err) {
         console.log(err)
+      } else {
+        conn.createChannel(function (err, ch) {
+          if (err) {
+            console.log(err)
+          } else {
+            const scheduleQueue = 'schedule'
+            ch.assertQueue(scheduleQueue, { durable: false })
+            console.log('waiting for new claims')
+
+            ch.consume(scheduleQueue, function (msg) {
+              console.log(`claim received for scheduling - ${msg.content.toString()}`)
+              scheduleService.create(JSON.parse(msg.content))
+            }, { noAck: true })
+
+            const valueQueue = 'value'
+            ch.assertQueue(valueQueue, { durable: false })
+            console.log('waiting for new calculation')
+
+            ch.consume(valueQueue, function (msg) {
+              console.log(`calculation received for payment - ${msg.content.toString()}`)
+              scheduleService.updateValue(JSON.parse(msg.content))
+            }, { noAck: true })
+          }
+        })
       }
-      conn.createChannel(function (err, ch) {
-        if (err) {
-          console.log(err)
-        }
-
-        const scheduleQueue = 'schedule'
-        ch.assertQueue(scheduleQueue, { durable: false })
-        console.log('waiting for new claims')
-
-        ch.consume(scheduleQueue, function (msg) {
-          console.log(`claim received for scheduling - ${msg.content.toString()}`)
-          scheduleService.create(JSON.parse(msg.content))
-        }, { noAck: true })
-
-        const valueQueue = 'value'
-        ch.assertQueue(valueQueue, { durable: false })
-        console.log('waiting for new calculation')
-
-        ch.consume(valueQueue, function (msg) {
-          console.log(`calculation received for payment - ${msg.content.toString()}`)
-          scheduleService.updateValue(JSON.parse(msg.content))
-        }, { noAck: true })
-      })
     })
   }
 }
