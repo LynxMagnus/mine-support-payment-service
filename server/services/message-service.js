2d9ce6aff4756e9d94c3c9a2fe0ffc23bc267752
Matthew Collins <matthew.collins@defra.gov.uk>
2019-07-22 08:23:12 +0100

    Change message queue library from amqplib to rhea

--- server/services/message-service.js
+++ server/services/message-service.js
@@ -1,38 +1,32 @@
-const amqp = require('amqplib/callback_api')
-const scheduleService = require('./schedule-service')
+const container = require('rhea')
 const config = require('../config')
+const scheduleService = require('./schedule-service')
 
 module.exports = {
   receiveSchedule: function () {
-    const messageQueue = config.messageQueue
-    amqp.connect(messageQueue, function (err, conn) {
-      if (err) {
-        console.log(err)
-      } else {
-        conn.createChannel(function (err, ch) {
-          if (err) {
-            console.log(err)
-          } else {
-            const scheduleQueue = 'schedule'
-            ch.assertQueue(scheduleQueue, { durable: false })
-            console.log('waiting for new claims')
-
-            ch.consume(scheduleQueue, function (msg) {
-              console.log(`claim received for scheduling - ${msg.content.toString()}`)
-              scheduleService.create(JSON.parse(msg.content))
-            }, { noAck: true })
-
-            const valueQueue = 'value'
-            ch.assertQueue(valueQueue, { durable: false })
-            console.log('waiting for new calculation')
-
-            ch.consume(valueQueue, function (msg) {
-              console.log(`calculation received for payment - ${msg.content.toString()}`)
-              scheduleService.updateValue(JSON.parse(msg.content))
-            }, { noAck: true })
-          }
-        })
-      }
+    const scheduleQueue = 'schedule'
+    const valueQueue = 'value'
+    const activeMQOptions = {
+      transport: config.messageQueuePort === 5672 ? 'tcp' : 'ssl',
+      port: config.messageQueuePort,
+      reconnect_limit: 10,
+      host: config.messageQueue,
+      hostname: config.messageQueue,
+      username: config.messageQueueUser,
+      password: config.messageQueuePass
+    }
+    container.on('connection_open', (context) => {
+      context.connection.open_receiver(valueQueue).on('message', (context) => {
+        console.log(`calculation received for payment - ${context.message.body}`)
+        scheduleService.updateValue(JSON.parse(context.message.body))
+        context.delivery.release({ undeliverable_here: true })
+      })
+      context.connection.open_receiver(scheduleQueue).on('message', (context) => {
+        console.log(`claim received for scheduling - ${context.message.body}`)
+        scheduleService.create(JSON.parse(context.message.body))
+        context.delivery.release({ undeliverable_here: true })
+      })
     })
+    container.connect(activeMQOptions)
   }
 }
