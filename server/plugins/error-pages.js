cc07a0cbfead39fada0fca89f2cf3d8a2c5b5c96
John Watson <john.watson2@rpa.gov.uk>
2019-08-02 14:10:19 +0100

    Updated error response handling

--- server/plugins/error-pages.js
+++ server/plugins/error-pages.js
@@ -1,3 +1,4 @@
+const processErrorResponse = require('./process-error-response')
 /*
 * Add an `onPreResponse` listener to return error pages
 */
@@ -7,29 +8,7 @@ module.exports = {
     name: 'error-pages',
     register: (server, options) => {
       server.ext('onPreResponse', (request, h) => {
-        const response = request.response
-
-        if (response.isBoom) {
-          // An error was raised during
-          // processing the request
-          const statusCode = response.output.statusCode
-
-          // In the event of 404
-          // return the `404` view
-          if (statusCode === 404) {
-            return h.view('404').code(statusCode)
-          }
-
-          request.log('error', {
-            statusCode: statusCode,
-            data: response.data,
-            message: response.message
-          })
-
-          // The return the `500` view
-          return h.view('500').code(statusCode)
-        }
-        return h.continue
+        return request.response.isBoom ? processErrorResponse(request) : h.continue
       })
     }
   }
