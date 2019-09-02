da36bce9b3209ade630c894eb3a94e4c923c48a6
John Watson <john.watson2@rpa.gov.uk>
2019-09-02 11:12:09 +0100

    Readiness probe unit tests updated

--- test/server/routes/healthy.test.js
+++ test/server/routes/healthy.test.js
@@ -2,10 +2,13 @@ describe('Healthy test', () => {
   let createServer
   let server
   let databaseService
+  let connectionService
 
   beforeAll(async () => {
     jest.mock('../../../server/services/database-service')
+    jest.mock('../../../server/services/connection-service')
     databaseService = require('../../../server/services/database-service')
+    connectionService = require('../../../server/services/connection-service')
     createServer = require('../../../server')
   })
 
@@ -21,6 +24,7 @@ describe('Healthy test', () => {
     }
 
     databaseService.isConnected = jest.fn(() => true)
+    connectionService.isConnected = jest.fn(() => true)
 
     const response = await server.inject(options)
     expect(response.statusCode).toBe(200)
@@ -33,6 +37,33 @@ describe('Healthy test', () => {
     }
 
     databaseService.isConnected = jest.fn(() => false)
+    connectionService.isConnected = jest.fn(() => true)
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(500)
+  })
+
+  test('GET /healthy route returns error if message queue unavailable', async () => {
+    const options = {
+      method: 'GET',
+      url: '/healthy'
+    }
+
+    databaseService.isConnected = jest.fn(() => true)
+    connectionService.isConnected = jest.fn(() => false)
+
+    const response = await server.inject(options)
+    expect(response.statusCode).toBe(500)
+  })
+
+  test('GET /healthy route returns error if message queue and database unavailable', async () => {
+    const options = {
+      method: 'GET',
+      url: '/healthy'
+    }
+
+    databaseService.isConnected = jest.fn(() => false)
+    connectionService.isConnected = jest.fn(() => false)
 
     const response = await server.inject(options)
     expect(response.statusCode).toBe(500)
