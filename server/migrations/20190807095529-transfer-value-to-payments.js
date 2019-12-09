1e11ab2ed37e6e621c1f12fbe5c8280bcd9899fe
John Watson <42574977+johnwatson484@users.noreply.github.com>
2019-12-09 14:57:22 +0000

    Fix migration which does not retain data (#34)
    https://eaflood.atlassian.net/browse/PSD-1

--- server/migrations/20190807095529-transfer-value-to-payments.js
+++ server/migrations/20190807095529-transfer-value-to-payments.js
@@ -2,24 +2,50 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const [payments] = await queryInterface.sequelize.query('SELECT DISTINCT "claimId", value FROM public."schedules";')
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      const [payments] = await queryInterface.sequelize.query('SELECT DISTINCT "claimId", value FROM public."schedules";')
 
-    if (payments.length) {
-      const transformed = payments.map(({ claimId, value }) => ({
-        claimId,
-        value,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-      queryInterface.bulkInsert('payments', transformed)
+      if (payments.length) {
+        const transformed = payments.map(({ claimId, value }) => ({
+          claimId,
+          value,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }))
+        await queryInterface.bulkInsert('payments', transformed, { transaction })
+      }
+      await queryInterface.removeColumn('schedules', 'value', { transaction })
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
     }
-
-    queryInterface.removeColumn('schedules', 'value')
   },
-
   down: async (queryInterface, Sequelize) => {
-    queryInterface.addColumn('schedules', 'value', {
-      type: Sequelize.DECIMAL
-    })
+    const transaction = await queryInterface.sequelize.transaction()
+    try {
+      await queryInterface.addColumn('schedules', 'value', {
+        type: Sequelize.DECIMAL
+      },
+      { transaction })
+
+      const [payments] = await queryInterface.sequelize.query('SELECT DISTINCT "claimId", value FROM public."payments";')
+
+      await Promise.all(
+        payments.map(async (payment) =>
+          queryInterface.bulkUpdate('schedules', {
+            value: payment.value
+          }, {
+            claimId: payment.claimId
+          },
+          { transaction })
+        )
+      )
+      await transaction.commit()
+    } catch (err) {
+      await transaction.rollback()
+      throw err
+    }
   }
 }
