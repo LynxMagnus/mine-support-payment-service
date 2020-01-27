2a8124ac17dfbc15c065bb3f3d9ded363c55efef
John Watson <john.watson2@rpa.gov.uk>
2020-01-27 15:47:51 +0000

    Test remove MQ

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ async function createServer () {
     await server.register(require('./plugins/logging'))
   }
 
-  await messageService.registerService()
+  // await messageService.registerService()
 
   return server
 }
