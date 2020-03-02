ee5abd4893c1b52caa8e87c557815c9e06a883f8
Paul Andrews <paul.andrews@defra.gov.uk>
2020-03-02 16:18:28 +0000

    config schema 4

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -2,7 +2,7 @@ const dbConfig = {
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB || 'mine_payments',
-  schema: process.env.POSTGRES_SCHEMA || '',
+  schema: process.env.POSTGRES_SCHEMA || 'pr53',
   host: process.env.POSTGRES_HOST || 'ffc-demo-payment-postgres',
   port: process.env.POSTGRES_PORT || 5432,
   dialect: 'postgres'
