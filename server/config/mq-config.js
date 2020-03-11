de49b27d0641bd00903c8997e734ee9d520bb6eb
markysoft <mark.harrop@defra.gov.uk>
2020-03-11 08:30:46 +0000

    simplifiy env vars

--- server/config/mq-config.js
+++ server/config/mq-config.js
@@ -1,31 +1,25 @@
 const joi = require('@hapi/joi')
 
+const queueSchema = joi.object({
+  name: joi.string().required(),
+  endpoint: joi.string().required(),
+  queueUrl: joi.string().required(),
+  region: joi.string().default('eu-west-2'),
+  accessKeyId: joi.string().optional(),
+  secretAccessKey: joi.string().optional(),
+  createQueue: joi.bool().default(true)
+})
+
 const mqSchema = joi.object({
-  scheduleQueue: {
-    name: joi.string().default('schedule'),
-    endpoint: joi.string().default('http://localhost:9324'),
-    queueUrl: joi.string().default('http://localhost:9324/queue/schedule'),
-    region: joi.string().default('eu-west-2'),
-    accessKeyId: joi.string(),
-    secretAccessKey: joi.string(),
-    createQueue: joi.bool().default(true)
-  },
-  paymentQueue: {
-    name: joi.string().default('payment'),
-    endpoint: joi.string().default('http://localhost:9324'),
-    queueUrl: joi.string().default('http://localhost:9324/queue/payment'),
-    region: joi.string().default('eu-west-2'),
-    accessKeyId: joi.string(),
-    secretAccessKey: joi.string(),
-    createQueue: joi.bool().default(true)
-  }
+  scheduleQueue: queueSchema,
+  paymentQueue: queueSchema
 })
 
 const mqConfig = {
   scheduleQueue: {
     name: process.env.SCHEDULE_QUEUE_NAME,
     endpoint: process.env.SCHEDULE_ENDPOINT,
-    queueUrl: process.env.SCHEDULE_QUEUE_URL,
+    queueUrl: process.env.SCHEDULE_QUEUE_URL || `${process.env.SCHEDULE_ENDPOINT}/${process.env.SCHEDULE_QUEUE_NAME}`,
     region: process.env.SCHEDULE_QUEUE_REGION,
     accessKeyId: process.env.SCHEDULE_QUEUE_ACCESS_KEY_ID,
     secretAccessKey: process.env.SCHEDULE_QUEUE_ACCESS_KEY,
@@ -34,7 +28,7 @@ const mqConfig = {
   paymentQueue: {
     name: process.env.PAYMENT_QUEUE_NAME,
     endpoint: process.env.PAYMENT_ENDPOINT,
-    queueUrl: process.env.PAYMENT_QUEUE_URL,
+    queueUrl: process.env.PAYMENT_QUEUE_URL || `${process.env.PAYMENT_ENDPOINT}/${process.env.PAYMENT_QUEUE_NAME}`,
     region: process.env.PAYMENT_QUEUE_REGION,
     accessKeyId: process.env.PAYMENT_QUEUE_ACCESS_KEY_ID,
     secretAccessKey: process.env.PAYMENT_QUEUE_ACCESS_KEY,
@@ -51,7 +45,12 @@ if (mqResult.error) {
   throw new Error(`The message queue config is invalid. ${mqResult.error.message}`)
 }
 
-const scheduleQueueConfig = { ...mqResult.value.scheduleQueue }
-const paymentQueueConfig = { ...mqResult.value.paymentQueue }
+const scheduleQueueConfig = mqResult.value.scheduleQueue
+const paymentQueueConfig = mqResult.value.paymentQueue
+console.log('**************', scheduleQueueConfig)
+console.log('**************', paymentQueueConfig)
 
-module.exports = { paymentQueueConfig, scheduleQueueConfig }
+module.exports = {
+  paymentQueueConfig,
+  scheduleQueueConfig
+}
