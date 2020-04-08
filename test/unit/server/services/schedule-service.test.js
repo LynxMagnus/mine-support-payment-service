3a7e66db12c62f61aa014b59134e47fb55157f27
John Watson <42574977+johnwatson484@users.noreply.github.com>
2020-04-08 13:48:37 +0100

    Scheduling updated to accept start date (#74)

--- test/unit/server/services/schedule-service.test.js
+++ test/unit/server/services/schedule-service.test.js
@@ -16,10 +16,16 @@ describe('Schedule service tests', () => {
       claimId: 'MINE003'
     }
     const spy = jest.spyOn(mockScheduleRespository, 'create')
-
-    await scheduleService.create(claim)
+    const startDate = new Date(2020, 3, 7)
+    await scheduleService.create(claim, startDate)
 
     expect(spy).toHaveBeenCalledTimes(6)
+    expect(spy).toHaveBeenCalledWith({ claimId: claim.claimId, paymentDate: new Date(2020, 4, 1) })
+    expect(spy).toHaveBeenCalledWith({ claimId: claim.claimId, paymentDate: new Date(2020, 5, 1) })
+    expect(spy).toHaveBeenCalledWith({ claimId: claim.claimId, paymentDate: new Date(2020, 6, 1) })
+    expect(spy).toHaveBeenCalledWith({ claimId: claim.claimId, paymentDate: new Date(2020, 7, 1) })
+    expect(spy).toHaveBeenCalledWith({ claimId: claim.claimId, paymentDate: new Date(2020, 8, 1) })
+    expect(spy).toHaveBeenCalledWith({ claimId: claim.claimId, paymentDate: new Date(2020, 9, 1) })
     spy.mockRestore()
     done()
   })
