f01208959e6a18499868e4628cd2c8692ce986ca
Mark Harrop <mark.harrop@defra.gov.uk>
2020-06-15 14:00:06 +0100

    remove message service check from healthy end point Whether the API is ready is only dependent on the database

--- server/routes/healthy.js
+++ server/routes/healthy.js
@@ -1,15 +1,22 @@
 const databaseService = require('../services/database-service')
-const messageService = require('../services/message-service')
+
+const SERVICE_UNAVAILABLE = 503
+const OK = 200
 
 module.exports = {
   method: 'GET',
   path: '/healthy',
   options: {
     handler: async (request, h) => {
-      if (await databaseService.isConnected() && messageService.isRunning()) {
-        return h.response('ok').code(200)
+      try {
+        if (await databaseService.isConnected()) {
+          return h.response('ok').code(OK)
+        }
+        return h.response('service unavailable').code(SERVICE_UNAVAILABLE)
+      } catch (ex) {
+        console.error('error running healthy check', ex)
+        return h.response(`error running healthy check: ${ex.message}`).code(SERVICE_UNAVAILABLE)
       }
-      return h.response('services unavailable').code(500)
     }
   }
 }
