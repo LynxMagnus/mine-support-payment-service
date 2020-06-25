8f4ca7112b21729b9547495af3bf7e39cd0b7311
Mark Harrop <mark.harrop@defra.gov.uk>
2020-06-25 13:34:33 +0100

    move app instights load to top of index.js auto collect console output

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const createServer = require('./server')
-
 require('./server/services/app-insights').setup()
+const createServer = require('./server')
 
 createServer()
   .then(server => server.start())
--- server/services/app-insights.js
+++ server/services/app-insights.js
@@ -2,9 +2,14 @@ const appInsights = require('applicationinsights')
 
 function setup () {
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
-    appInsights.setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY).start()
+    appInsights
+      .setup(process.env.APPINSIGHTS_INSTRUMENTATIONKEY)
+      .setAutoCollectConsole(true, true)
+      .start()
+
+    const cloudRoleTag = appInsights.defaultClient.context.keys.cloudRole
     const appName = process.env.APPINSIGHTS_CLOUDROLE
-    appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] = appName
+    appInsights.defaultClient.context.tags[cloudRoleTag] = appName
   }
 }
 
