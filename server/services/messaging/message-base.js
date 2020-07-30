6e69bb2f90669bc154518b2951b6c69c8d06373a
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2020-07-30 17:49:55 +0100

    fix: no need to send action to base class

--- server/services/messaging/message-base.js
+++ server/services/messaging/message-base.js
@@ -1,7 +1,7 @@
 const { ServiceBusClient } = require('@azure/service-bus')
 
 class MessageBase {
-  constructor (name, config, credentials, action) {
+  constructor (name, config, credentials) {
     this.name = name
     this.sbClient = credentials ? ServiceBusClient.createFromAadTokenCredentials(config.host, credentials) : ServiceBusClient.createFromConnectionString(`Endpoint=sb://${config.host}/;SharedAccessKeyName=${config.username};SharedAccessKey=${config.password}`)
     this.queueClient = this.sbClient.createQueueClient(config.address)
