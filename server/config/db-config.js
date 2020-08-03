cc1b9ad0208a7ea9b441db7c69b2168c584260ab
Mark Harrop <mark.harrop@defra.gov.uk>
2020-08-03 13:30:33 +0100

    add schema env var

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -1,6 +1,7 @@
 const dbConfig = {
   username: process.env.POSTGRES_USERNAME,
   password: process.env.POSTGRES_PASSWORD,
+  schema: process.env.POSTGRES_SCHEMA || 'public',
   database: process.env.POSTGRES_DB || 'mine_payments',
   host: process.env.POSTGRES_HOST || 'ffc-demo-payment-postgres',
   port: process.env.POSTGRES_PORT || 5432,
