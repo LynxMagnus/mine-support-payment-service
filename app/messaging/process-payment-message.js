5b4d764f8d6bd12ae3dc6f11b5725668fe749f87
John Watson <johnwatson484@hotmail.com>
2021-02-18 12:37:02 +0000

    Linkerd reset

--- app/messaging/process-payment-message.js
+++ app/messaging/process-payment-message.js
@@ -4,8 +4,8 @@ const sendEvent = require('../events')
 async function processPaymentMessage (message, receiver) {
   try {
     await createPayment(message.body)
-    await receiver.completeMessage(message)
     await sendEvent(message.body, 'uk.gov.demo.claim.payment.attached')
+    await receiver.completeMessage(message)
   } catch (err) {
     console.error('Unable to process message:', err)
     await receiver.abandonMessage(message)
--- app/messaging/process-schedule-message.js
+++ app/messaging/process-schedule-message.js
@@ -5,8 +5,8 @@ async function processScheduleMessage (message, receiver) {
   try {
     const scheduleStartDate = new Date()
     await createSchedule(message.body, scheduleStartDate)
-    await sendEvent(message.body, 'uk.gov.demo.claim.payment.scheduled')
     await receiver.completeMessage(message)
+    await sendEvent(message.body, 'uk.gov.demo.claim.payment.scheduled')
   } catch (err) {
     console.error('Unable to process message:', err)
     await receiver.abandonMessage(message)
